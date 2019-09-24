a7cf271b770280abcc6d731a5d4ef34d34bcff73
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:38:08 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan10/logika/PBO10118080Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program Logika umur
+ */
+public class PBO10118080Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur =19;
+        boolean dewasa;
+        dewasa=(umur >=17);
+        System.out.println("Status Dewasa : "+dewasa);
+    }
+    
+}
